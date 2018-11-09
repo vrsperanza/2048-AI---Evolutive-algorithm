9627f5a44b0f6619ae928702a71203af1b0b1d92
Vitor Rossi Speranza <vrsperanza@gmail.com>
2018-11-09 17:26:41 -0200

    Update bot.cpp

--- bot.cpp
+++ bot.cpp
@@ -10,12 +10,13 @@
 
 using namespace std;
 
-int populationSize;
-double startGenerationImportance;
-int generationImportanceHalfTime;
-int trainingIterations;
-double currGenerationImportance;
-const int hiddenLayerSize = 2;
+const int populationSize = 32;
+const double startGenerationImportance = 0.01;
+const int generationImportanceHalfTime = -1;
+const int trainingIterations = 5000;
+const int hiddenLayerSize = 3;
+
+double currGenerationImportance = startGenerationImportance;
 
 default_random_engine generator;
 normal_distribution<double> distribution(0,1);
@@ -51,7 +52,7 @@ class bot{
                 }
             }
             
-			result += (tot + l0Offset[l]) > 0;
+			result += l1[l] * ((tot + l0Offset[l]) > 0);
         }
 		return result;
 	}
@@ -119,6 +120,16 @@ class bot{
         averageScore = 0;
     }
 
+    void clone(bot parent){
+        for(int i = 0; i < hiddenLayerSize; i++){
+            for(int j = 0; j < 16; j++)
+                for(int k = 0; k < 16; k++)
+                    l0[i][j][k] = parent.l0[i][j][k];
+            l0Offset[i] = parent.l0Offset[i];
+            l1[i] = parent.l1[i];
+        }
+    }
+
     // Simula um jogo e adiciona esse jogo a pontuação
     // atual do robo, de acordo com a importância da geração atual
     void updateScore(){
@@ -134,15 +145,20 @@ class bot{
     // Ao cruzar seleciona aleatóriamente quanto do pai ou da mãe será
     // e soma uma mutação a cada peso de cada neurônio
     void crossOver(const bot & a, const bot & b, double mutationSize){
-        for(int i = 0; i < hiddenLayerSize; i++){
-            double aWeight = fRand();
-            for(int j = 0; j < 16; j++)
-                for(int k = 0; k < 16; k++)
-                    l0[i][j][k] = (aWeight * a.l0[i][j][k] + (1 - aWeight) * b.l0[i][j][k]) + mutationSize * distribution(generator) * distribution(generator);
-            l0Offset[i] = (aWeight * a.l0Offset[i] + (1 - aWeight) * b.l0Offset[i]) + mutationSize * distribution(generator) * distribution(generator);
+        //if(fRand() < 0.5){
+        //    clone(b);
+        //}
+        //else{
+            for(int i = 0; i < hiddenLayerSize; i++){
+                double aWeight = fRand();
+                for(int j = 0; j < 16; j++)
+                    for(int k = 0; k < 16; k++)
+                        l0[i][j][k] = (aWeight * a.l0[i][j][k] + (1 - aWeight) * b.l0[i][j][k]) + mutationSize * distribution(generator) * distribution(generator);
+                l0Offset[i] = (aWeight * a.l0Offset[i] + (1 - aWeight) * b.l0Offset[i]) + mutationSize * distribution(generator) * distribution(generator);
 
-            l1[i] = (aWeight * a.l1[i] + (1 - aWeight) * b.l1[i]) + mutationSize * distribution(generator) * distribution(generator);
-        }
+                l1[i] = (aWeight * a.l1[i] + (1 - aWeight) * b.l1[i]) + mutationSize * distribution(generator) * distribution(generator);
+            }
+        //}
     }
 
 	bool operator < (const bot& b) const {
@@ -150,26 +166,14 @@ class bot{
 	}
 };
 
-vector<bot> bots;
+vector<bot> bots(populationSize);
 
 void updateScore(int botId){
 	bots[botId].updateScore();
 }
 
 int main(){
-    cout << "Population size = ? (default 32)" << endl;
-    cin >> populationSize;
-    cout << "Start generation importance = ? (default 0.01)" << endl;
-    cin >> startGenerationImportance;
-    cout << "Generation importance half time = ? (default -1 (disabled))" << endl;
-    cin >> generationImportanceHalfTime;
-    cout << "Training iterations = ? (default 5000)" << endl;
-    cin >> trainingIterations;
-
     srand(time(NULL));
-
-	currGenerationImportance = startGenerationImportance;
-    bots.resize(populationSize);
 
     int gens = 0;
 
@@ -180,7 +184,7 @@ int main(){
 	
 	double bestScore = 0;
 	
-    while(gens <= trainingIterations){
+    while(gens != trainingIterations){
         // Importancia da geração atual diminui com o tempo
 		if(generationImportanceHalfTime > 0)
 			currGenerationImportance = startGenerationImportance * generationImportanceHalfTime/double(generationImportanceHalfTime+gens);
@@ -213,7 +217,7 @@ int main(){
         // o melhor tendo 50% de chance de ser escolhido,
         // o segundo melhor 25% e assim por diante.
         // A outra metade é substituida por seus filhos
-        for(int i = 0; i < 0.5 * bots.size(); i++){
+        for(int i = 0; i < 0.75 * bots.size(); i++){
             int r = rand();
             int nxt = bots.size()-1;
             while(r & 1){
